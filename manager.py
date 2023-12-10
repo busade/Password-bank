1bff3c1e2d54929b4f9b62a5dbd0cd5d990e38f5
Adesola Busari <adesolaisa3@gmail.com>
2023-12-10 10:02:52 +0100

    included the search button

--- manager.py
+++ manager.py
@@ -1,18 +1,14 @@
 from tkinter import *
+import string
 from tkinter import messagebox
 from random import choice, randint, shuffle
 
 
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    password_letter = [choice(letters) for _ in range(randint(8, 10))]
-    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
-    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_letter = [choice(string.ascii_letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(string.punctuation) for _ in range(randint(2, 4))]
+    password_numbers = [choice(string.hexdigits) for _ in range(randint(2, 4))]
     password_list = password_letter + password_symbols + password_numbers
     shuffle(password_list)
 
@@ -38,7 +34,35 @@ def save():
 
 
 
+# def view():
+#     index = 0
+#     arg = search.get()
 
+ 
+#     with open("doc.txt","r") as f:
+#         for line_number, line in enumerate(f,start = 0):
+#             columns= line.strip().split(",")
+#             values= columns[index]
+#             print("values")
+                
+#             if arg in values:
+#                 for idx, value  in enumerate(columns, start=0):
+#                     details={"key":idx,"value":value}
+#                     return messagebox.showinfo(title="details", message= f"This are the details provided for {details[0]}\n Email:{details[1]}\n Password: {details[2]}")
+
+def view():
+    arg = search.get()
+
+    with open("doc.txt", "r") as f:
+        for line_number, line in enumerate(f, start=0):
+            columns = line.strip().split("|")
+
+            if arg in columns[0]:  # Assuming the website is in the first column
+                messagebox.showinfo(
+                    title="details",
+                    message=f"This are the details provided for {columns[0]}\n Email:{columns[1]}\n Password: {columns[2]}",
+                )
+    
 
 
 window= Tk()
@@ -46,7 +70,7 @@ window= Tk()
 window.title = ("Password manager and Genrator")
 window.config(padx=20,pady=20)
 
-canvas= Canvas( height=200, width=200, background='white')
+canvas= Canvas( height=150, width=150, background='white')
 logo_img = PhotoImage(file ="log.png")
 
 canvas.create_image(100,100, image = logo_img)
@@ -78,6 +102,12 @@ pass_generator.grid(column=2, row=4)
 
 addition = Button(text='Add', width=36 , command = save)
 addition.grid(column=1, row=5, columnspan=2)
+
+search = Entry(width=35)
+search.grid(column=1,row=6)
+
+search_button= Button(text="Search", command=view)
+search_button.grid(column=2,row=6)
 
 
 
